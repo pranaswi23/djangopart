e582c592161ad1d26e4978e2107bb43d62dc0a36
pranaswi23 <pranaswireddy99@gmail.com>
2020-06-25 15:59:48 +0530

    added dummy comment in views.py for testing

--- dfrontend/frontend/views.py
+++ dfrontend/frontend/views.py
@@ -6,3 +6,4 @@ def fe (request):
     return HttpResponse('<h1> Age classification using speech processing </h1><form action="" methods=""><label for="name">Name*:</label> <input type="text" id="name" name=""size="50"></br></br><label for="email">Email Id*:</label> <input type="text" id="email" email=""size="50"></br></br><label for="phno">Mobile No*:</label> <input type="text" id="phno" phno=""size="20"></br></br><label for="file">Choose file to upload</label><input type="file" id="file" name="file" multiple></br></br><input type="submit" value="Submit"></br></br><label for="output">Output:</label> <input type="text" id="output" output=""size="20"></form>')
 
 # Create your views here.
+# testing my own repo
